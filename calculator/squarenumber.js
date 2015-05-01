da13269da386a845d0e2feacab19db78badd5c5d
unknown <AnnaE@ncmls8065.ncmls.org>
2015-04-30 22:50:32 -0400

    finished part 1 of calculator.

---
+++ calculator/squarenumber.js
@@ -0,0 +1,8 @@
+function squareNumber(num) {
+  var square = num * num;
+  console.log('The result of squaring the number '
+              + num + ' is ' + square + '.');
+  return square;
+}
+squareNumber(4)
+squareNumber(5)
